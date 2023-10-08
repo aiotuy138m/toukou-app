62677fe5c09cd7e5ee75423f85eaad35a81a8730
aiotuy138m <122207424+aiotuy138m@users.noreply.github.com>
2023-10-08 23:37:42 +0900

    [変更]typeScriptの記述をコメント化、app.tsはapp.jsに戻した

--- vite.config.js
+++ vite.config.js
@@ -11,8 +11,8 @@ export default defineConfig({
     plugins: [
         laravel({
             input: [
-                'resources/css/app.scss',
-                'resources/js/app.ts',
+                'resources/css/app.css',
+                'resources/js/app.js',
             ],
             refresh: true,
         }),
